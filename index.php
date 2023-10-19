dc7f5c3b5e2c0cf53a55f4adf93153eb12d0fa50
Francis Antonio <113699783+Fantonio27@users.noreply.github.com>
2023-10-19 18:27:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -40,23 +40,22 @@ include './crud/db.php';
                     </thead>
                     <tbody>
                         <?php
-                        include './crud/read.php';
+                            include './crud/read.php';
                         ?>
                     </tbody>
                 </table>
             </div>
-
-            <?php
-                include './modals.php';
-            ?>
-
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-                crossorigin="anonymous"></script>
-
-            <script src="./script/index.js"></script>
         </div>
     </center>
-</body>
+    
+    <?php
+        include './modals.php';
+    ?>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"></script>
+
+    <script src="./script/index.js"></script>
+</body>
 </html>
